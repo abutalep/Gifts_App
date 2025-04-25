89bcea283d0d1718e87008c8d29252b58cd442b3
AbuTalep <ahmedabutalep10@gmail.com>
2025-04-25 22:30:31 +0300

    Add Splash Screen

---
+++ lib/views/screens/splashScreen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:gifts_app/views/screens/onboardingOne.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+   void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingOne()),
+      );
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset(
+    "assets/images/Splash_Screen.png",
+    fit: BoxFit.cover,
+    width: double.infinity,
+    height: double.infinity,
+  ),
+    );
+  }
+}
